b1b37b222081e90760aa788f3cef595f5649627f
Hazget <hazget91@gmail.com>
2022-02-14 16:46:21 +0300

    first com

---
+++ src/pages/Main.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Welcome to App</h1>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/pages/UserPage.vue
@@ -0,0 +1,147 @@
+<template>
+  <div>
+    <h1>Page With Notes</h1>
+    <my-input
+      v-model="searchQuery"
+      placeholder="Searching...."
+      v-focus
+    />
+    <div class="app__btns">
+      <my-button
+        @click="showDialog"
+      >
+        Create User
+      </my-button>
+      <my-select
+        v-model="selectedSort"
+        :options="sortOptions"
+      />
+    </div>
+    <my-dialog v-model:show="dialogVisible">
+      <post-form
+        @create="createPost"
+      />
+    </my-dialog>
+    <post-list
+      :posts="sortedAndSearchedPosts"
+      @remove="removePost"
+      v-if="!isPostsLoading"
+    />
+    <div v-else>Loading...</div>
+    <div v-intersection="loadMorePosts" class="observer"></div>
+  </div>
+</template>
+
+<script>
+import PostForm from "@/components/PostForm";
+import PostList from "@/components/PostList";
+import MyButton from "@/components/UI/MyButton";
+import axios from 'axios';
+import MySelect from "@/components/UI/MySelect";
+import MyInput from "@/components/UI/MyInput";
+export default {
+  components: {
+    MyInput,
+    MySelect,
+    MyButton,
+    PostList, PostForm
+  },
+  data() {
+    return {
+      posts: [],
+      dialogVisible: false,
+      isPostsLoading: false,
+      selectedSort: '',
+      searchQuery: '',
+      page: 1,
+      limit: 10,
+      totalPages: 0,
+      sortOptions: [
+        {value: 'title', name: 'with Title'},
+        {value: 'body', name: 'With Content'},
+      ]
+    }
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post);
+      this.dialogVisible = false;
+    },
+    removePost(post) {
+      this.posts = this.posts.filter(p => p.id !== post.id)
+    },
+    showDialog() {
+      this.dialogVisible = true;
+    },
+    async fetchPosts() {
+      try {
+        this.isPostsLoading = true;
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+          params: {
+            _page: this.page,
+            _limit: this.limit
+          }
+        });
+        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit)
+        this.posts = response.data;
+      } catch (e) {
+        alert('Error')
+      } finally {
+        this.isPostsLoading = false;
+      }
+    },
+    async loadMorePosts() {
+      try {
+        this.page += 1;
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+          params: {
+            _page: this.page,
+            _limit: this.limit
+          }
+        });
+        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit)
+        this.posts = [...this.posts, ...response.data];
+      } catch (e) {
+        alert('Error')
+      }
+    }
+  },
+  mounted() {
+    this.fetchPosts();
+   
+  },
+  computed: {
+    sortedPosts() {
+      return [...this.posts].sort((post1, post2) => post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]))
+    },
+    sortedAndSearchedPosts() {
+      return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
+    }
+  },
+  watch: {
+    
+  }
+}
+</script>
+<style>
+.app__btns {
+  margin: 15px 0;
+  display: flex;
+  justify-content: space-between;
+}
+.page__wrapper {
+  display: flex;
+  margin-top: 15px;
+}
+.page {
+  border: 1px solid black;
+  padding: 10px;
+}
+.current-page {
+  border: 2px solid teal;
+}
+.observer {
+  height: 30px;
+  background: green;
+}
+</style>
